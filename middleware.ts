d800ccd39ca1b3db5fb6baa40555e5e3ff841c3d
suhel <116453294+suhel1519@users.noreply.github.com>
2024-07-24 18:49:07 +0530

    feat: clerk auth is added

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
+export default authMiddleware({
+  publicRoutes: ['/api/webhooks/clerk']
+})
+
 const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)", "/"]);
 
 export default clerkMiddleware((auth, request) => {
