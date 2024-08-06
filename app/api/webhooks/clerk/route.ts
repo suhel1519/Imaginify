21636a1d888fa0f5a4f92dcd19a9b5e2f9395d56
suhel <116453294+suhel1519@users.noreply.github.com>
2024-08-06 12:04:00 +0530

    logs added

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -9,6 +9,8 @@ export async function POST(req: Request) {
   // You can find this in the Clerk Dashboard -> Webhooks -> choose the webhook
   const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET;
 
+  console.log("WEBHOOK_SECRET called", WEBHOOK_SECRET);
+
   if (!WEBHOOK_SECRET) {
     throw new Error(
       "Please add WEBHOOK_SECRET from Clerk Dashboard to .env or .env.local"
@@ -30,7 +32,9 @@ export async function POST(req: Request) {
 
   // Get the body
   const payload = await req.json();
+  console.log("payload", payload);
   const body = JSON.stringify(payload);
+  console.log("body", body);
 
   // Create a new Svix instance with your secret.
   const wh = new Webhook(WEBHOOK_SECRET);
@@ -54,6 +58,9 @@ export async function POST(req: Request) {
   // Get the ID and type
   const { id } = evt.data;
   const eventType = evt.type;
+
+  console.log(`Webhook with and ID of ${id} and type of ${eventType}`);
+  console.log("Webhook body:", body);
 
   // CREATE
   if (eventType === "user.created") {
