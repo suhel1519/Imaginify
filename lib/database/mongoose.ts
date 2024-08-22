0dc0ad51d03e800cd1b68c4d4239437702f5d5a7
suhel <116453294+suhel1519@users.noreply.github.com>
2024-08-22 18:38:06 +0530

    db renamed

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -24,7 +24,7 @@ export const connectToDatabase = async () => {
   cached.promise =
     cached.promise ||
     mongoose.connect(MONGODB_URL, {
-      dbName: "photoshop",
+      dbName: "imaginify",
       bufferCommands: false,
     });
 
